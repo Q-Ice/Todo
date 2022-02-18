bf9e6b7bbc838b214e99f7a4d2033a2042ce51d0
绿叶 <78372813+Q-Ice@users.noreply.github.com>
2022-02-18 17:36:15 +0700

    Update todo.js

--- todo.js
+++ todo.js
@@ -10,35 +10,32 @@ const textTag = $('.number-task-list')
 const deleteTodo = $('.delete')
 const todo = $('.text-nt')
 
-let listTodo = []
-let listBoxCheck = []
-
-if (!localStorage.getItem('listTodo')) {
-    localStorage.setItem('listTodo', JSON.stringify(listTodo));
+let task = {
+    listTodo: [],
+    listBoxCheck: [],
 }
-if (!localStorage.getItem('listBoxCheck')) {
-    localStorage.setItem('listBoxCheck', JSON.stringify(listBoxCheck));
+
+if (!localStorage.getItem('Task')) {
+    localStorage.setItem('Task', JSON.stringify(task))
 }
 
 const app = {
     addTodo: function (item) {
-        listTodo.push(item)
-        listBoxCheck.push('box-check1')
-        localStorage.setItem('listTodo', JSON.stringify(listTodo));
-        localStorage.setItem('listBoxCheck', JSON.stringify(listBoxCheck));
+        task.listTodo.push(item)
+        task.listBoxCheck.push('box-check1')
+        localStorage.setItem('Task', JSON.stringify(task));
     },
     removeTodo: function (index) {
-        listTodo.splice(index, 1)
-        listBoxCheck.splice(index,1)
+        task.listTodo.splice(index, 1)
+        task.listBoxCheck.splice(index, 1)
         _this.handleButtonCirlce()
-        localStorage.setItem('listTodo', JSON.stringify(listTodo));
-        localStorage.setItem('listBoxCheck', JSON.stringify(listBoxCheck));
+        localStorage.setItem('Task', JSON.stringify(task));
     },
     render: function () {
-        const htmls = listTodo.map((item, index) => {
+        const htmls = task.listTodo.map((item, index) => {
             return `
                 <div class="number-todo" data-index="${index}">
-                    <div class="box-check ${listBoxCheck[index]}" data-index="${index}">
+                    <div class="box-check ${task.listBoxCheck[index]}" data-index="${index}">
                         <i class="check1 fa-solid fa-circle" data-index="${index}"></i>
                         <i class="check2 fa-solid fa-circle-check" data-index="${index}"></i>
                     </div>
@@ -51,11 +48,11 @@ const app = {
 
         list.innerHTML = htmls
 
-        const html2 = `Today <span class="number-task">(${listTodo.length})</span>`
+        const html2 = `Today <span class="number-task">(${task.listTodo.length})</span>`
         textTag.innerHTML = html2;
     },
     handleButtonCirlce: function () {
-        if (listTodo.length > 5) {
+        if (task.listTodo.length > 5) {
             btnCircle.style['position'] = 'sticky'
             btnCircle.style['float'] = 'right'
             btnCircle.style['bottom'] = '15px'
@@ -73,34 +70,25 @@ const app = {
             const index = deleteBtn.dataset.index
             this.removeTodo(index)
             this.render()
-            localStorage.setItem('listTodo', JSON.stringify(listTodo));
-            localStorage.setItem('listBoxCheck', JSON.stringify(listBoxCheck));
         }
         if (change && !deleteBtn) {
             const index2 = change.dataset.index
             const check = $(`.box-check[data-index="${index2}"]`)
             if (check.classList.contains('box-check1')) {
-                listBoxCheck[index2] = 'box-check2'
-                
+                task.listBoxCheck[index2] = 'box-check2'
+
             } else if (check.classList.contains('box-check2')) {
-                listBoxCheck[index2] = 'box-check1'
+                task.listBoxCheck[index2] = 'box-check1'
             }
             this.render()
         }
-        localStorage.setItem('listTodo', JSON.stringify(listTodo));
-        localStorage.setItem('listBoxCheck', JSON.stringify(listBoxCheck));
+        localStorage.setItem('Task', JSON.stringify(task));
     },
 
     init: function () {
         _this = this
-        if (!localStorage.getItem('listTodo')) {
-            localStorage.setItem('listTodo', JSON.stringify(listTodo));
-        }
-        if (!localStorage.getItem('listBoxCheck')) {
-            localStorage.setItem('listBoxCheck', JSON.stringify(listBoxCheck));
-        }
-        listTodo = JSON.parse(localStorage.getItem('listTodo')) || []
-        listBoxCheck = JSON.parse(localStorage.getItem('listBoxCheck')) || []
+        task.listTodo = JSON.parse(localStorage.getItem('Task')).listTodo || []
+        task.listBoxCheck = JSON.parse(localStorage.getItem('Task')).listBoxCheck || []
         submit.onclick = function () {
             const item = areaText.value
             if (!item == '') {
